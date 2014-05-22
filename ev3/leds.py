4f21cfeb8aa90d9afa52fa3c4757c30ba79f02aa
Xu Yuan <yuan.xu@dai-labor.de>
2014-05-23 00:56:39 +0200

    set_indicator

--- ev3/leds.py
+++ ev3/leds.py
@@ -3,8 +3,8 @@
 
 
 LEDS_PATH = '/sys/class/leds/ev3:'
-COLORS = ['green', 'red']
-NAMES = ['left', 'right']
+COLORS = {'green', 'red'}
+NAMES = {'left', 'right'}
 
 
 def _set_trigger(color, name, trigger):
@@ -20,31 +20,18 @@ def _set_trigger(color, name, trigger):
         f.write(trigger)
 
 
-def set_color(name, color):
-    '''turn LED to given color
-    parameters
-    ----------
-        - name: name of LED, e.g. left, right or all
-        - color: {'green", 'yellow', 'red'}
-    '''
-    name_l = name.lower()
-    if name_l == 'all':
-        for n in NAMES:
-            set_color(n, color)
-        return
-
-    if name_l not in NAMES:
+def _set_trigger_ext(color, name, trigger):
+    if name not in NAMES:
         raise RuntimeError('unknow LED name: ' + name)
 
     if color:
-        color_l = color.lower()
-        if color_l == 'yellow':
+        if color == 'yellow':
             for c in COLORS:
-                _set_trigger(c, name, 'default-on')
-        elif color_l in COLORS:
+                _set_trigger(c, name, trigger)
+        elif color in COLORS:
             for c in COLORS:
-                if c == color_l:
-                    _set_trigger(c, name, 'default-on')
+                if c == color:
+                    _set_trigger(c, name, trigger)
                 else:
                     _set_trigger(c, name, 'none')
         else:
@@ -52,6 +39,45 @@ def set_color(name, color):
     else:
         for c in COLORS:
             _set_trigger(c, name, 'none')
+
+
+def set_color(name, color):
+    '''turn LED to given color
+    parameters
+    ----------
+        - name: name of LED, e.g. {'left', 'right', 'all'}
+        - color: {'green', 'yellow', 'red'}
+    '''
+    if name == 'all':
+        for n in NAMES:
+            set_color(n, color)
+        return
+
+    _set_trigger_ext(color, name, 'default-on')
+
+
+def set_indicator(name, indicator, color):
+    '''makes the LED blink when there is activity from indicator
+    parameters
+    ----------
+        - name: name of LED, e.g. {'left', 'right', 'all'}
+        - indicator: {'sd', 'cpu'}
+    '''
+    if name == 'all':
+        for n in NAMES:
+            set_indicator(n, indicator)
+        return
+
+    if name not in NAMES:
+        raise RuntimeError('unknow LED name: ' + name)
+
+    if indicator == 'sd':
+        _set_trigger_ext(color, name, 'mmc0')
+    elif indicator == 'cpu':
+        _set_trigger_ext(color, name, 'heartbeat')
+    else:
+        raise RuntimeError('unknow indicator name: ' + indicator)
+
 
 if __name__ == '__main__':
     from time import sleep
@@ -78,3 +104,7 @@ if __name__ == '__main__':
     print 'set_color all green'
     set_color('all', 'green')
     sleep(1)
+
+    print 'set left for SD card and right for CPU'
+    set_indicator('left', 'sd', 'yellow')
+    set_indicator('right', 'cpu', 'red')
