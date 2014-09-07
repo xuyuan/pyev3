3b172f7c2f0f5a45f2804607e484768e61013910
Xu Yuan <yuan.xu@dai-labor.de>
2014-09-07 23:15:26 +0200

    use touch sensor

--- example/guitar.py
+++ example/guitar.py
@@ -2,14 +2,24 @@
 '''
 
 import time
-from ev3.sensor import IRProx
+from ev3.sensor import IRProx, TouchSensor
 from ev3.sound import beep
 
 
 ir = IRProx()
+tc = TouchSensor()
+
+# C note from http://liutaiomottola.com/formulae/freqtab.htm
+note_frequency = [16, 33, 65, 130, 262, 523, 1047, 2093, 4186, 8372]
 
 while True:
     d = ir.proximity
-    print d
-    beep(d * 100, 100)
-    #time.sleep(0.2)
+    d = d / 9 + 1  # about 1 ~ 7
+    d = min(7, d)
+    f = note_frequency[d]
+    #print f
+    if tc.released:
+        beep(f)
+    else:
+        beep(0)
+    time.sleep(0.01)
