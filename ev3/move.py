eba78a43ec84e401126f28a06a43c02cbdea971a
Xu Yuan <yuan.xu@dai-labor.de>
2016-05-02 22:24:39 +0200

    twist and move forward

--- ev3/move.py
+++ ev3/move.py
@@ -2,15 +2,17 @@
 '''
 
 from motor import Motor
+from math import degrees
 
 
 class MoveTank(object):
     '''move base for differential-drive robot
     '''
-    def __init__(self, base_width, left_motor_port='B', right_motor_port='C'):
+    def __init__(self, wheel_radius=0.016, base_width=0.18, left_motor_port='B', right_motor_port='C'):
         self.left_motor = Motor(left_motor_port)
         self.right_motor = Motor(right_motor_port)
         self.base_width = base_width
+        self.wheel_radius = wheel_radius
         self._twist = [0, 0]
 
     @property
@@ -22,20 +24,30 @@ class MoveTank(object):
         if v[0] == 0 and v[1] == 0:
             self.stop()
         else:
-            rot = v[1]
-            lv = rot
-            rv = -rot
-            if abs(rot) < 100:
-                lv += v[0]
-                rv += v[0]
-            self.left_motor.run_mode = 'forever'
-            self.right_motor.run_mode = 'forever'
-            self.left_motor.speed_setpoint = lv
-            self.right_motor.speed_setpoint = rv
-            self.left_motor.run = 1
-            self.right_motor.run = 1
+            rot = v[1] * self.base_width / 2
+            lv = v[0] + rot
+            rv = v[0] - rot
+            self.left_motor.speed_setpoint = int(lv / self.wheel_radius)
+            self.right_motor.speed_setpoint = int(rv / self.wheel_radius)
+            self.left_motor.command = 'run-forever'
+            self.right_motor.command = 'run-forever'
         self._twist = v
 
+    def move(self, d):
+        ang = int(degrees(d / self.wheel_radius))
+        self.left_motor.position_setpoint = ang
+        self.right_motor.position_setpoint = ang
+        self.left_motor.command = 'run-to-rel-pos'
+        self.right_motor.command = 'run-to-rel-pos'
+
     def stop(self):
-        self.left_motor.run = 0
-        self.right_motor.run = 0
+        self.left_motor.command = 'stop'
+        self.right_motor.command = 'stop'
+
+if __name__ == '__main__':
+    import time
+    move = MoveTank()
+    #move.twist = [1, 0]
+    #time.sleep(5)
+    #move.stop()
+    move.move(0.05)
