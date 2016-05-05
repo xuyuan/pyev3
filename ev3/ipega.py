b6748aee81fbad408c3c2b3a6938e0be5a648f3a
Xu Yuan <yuan.xu@dai-labor.de>
2016-05-05 02:08:35 +0200

    simple version

--- ev3/ipega.py
+++ ev3/ipega.py
@@ -6,25 +6,6 @@ http://python-evdev.readthedocs.io/en/latest/
 '''
 
 import evdev
-import threading
-
-## Some helpers ##
-
-def scale(val, src, dst):
-    """
-    Scale the given value from the scale of src to the scale of dst.
-
-    val: float or int
-    src: tuple
-    dst: tuple
-
-    example: print scale(99, (0.0, 99.0), (-1.0, +1.0))
-    """
-    return (float(val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
-
-
-def scale_stick(value):
-    return scale(value,(0,255),(-100,100))
 
 
 class Ipega(object):
@@ -35,18 +16,14 @@ class Ipega(object):
             if device.name == name:
                 fn = device.fn
                 break
-        assert fn
+        if fn is None:
+            raise RuntimeError("can't find device: " + name)
         self.gamepad = evdev.InputDevice(fn)
-        self.thread = threading.Thread(target=self.run)
-        self.thread.daemon = True
-        self.thread.start()
 
     def run(self):
         for event in self.gamepad.read_loop():
-            print event
+            print event, (evdev.categorize(event))
 
 if __name__ == '__main__':
     ipega = Ipega()
-    import time
-    while True:
-        time.sleep(0.1)
+    ipega.run()
